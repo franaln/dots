f5a1a43e62c2cfc48b02d5ea84966b81af4f05e7
Francisco Alonso <franaln@gmail.com>
2018-06-11 18:21:23 -0300

    add go/back hdmi to menu

---
+++ tint2/.config/tint2/music-status.sh
@@ -0,0 +1,16 @@
+#/usr/bin/bash
+
+status=$(playerctl status 2>/dev/null)
+
+if [[ $? != 0 ]] ; then
+    echo -e "\uF04d"
+
+else
+
+    if [[ "$status" == "Playing" ]]; then
+        { echo "$(playerctl metadata title | awk -v len=25 '{ if (length($0) > len) print substr($0, 1, len-3) "..."; else print; }' | tr -d '&' | awk '{print "  " $0 " |"}')"; echo "<b>$(playerctl metadata artist)</b>"; } | tr "\n" " "
+
+    elif [[ "$status" == "Paused" ]] ; then
+        { echo "$(playerctl metadata title | awk -v len=25 '{ if (length($0) > len) print substr($0, 1, len-3) "..."; else print; }' | tr -d '&' | awk '{print "  " $0 " |"}')"; echo "<b>$(playerctl metadata artist)</b>"; } | tr "\n" " "
+    fi
+fi
